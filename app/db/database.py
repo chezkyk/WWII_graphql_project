3a0c5cc9e4bdbf7406408adbce7aac9ae14ed422
chezkyk <Y0548532123@GMAIL.COM>
2024-10-31 17:57:54 +0200

    added connection to database

---
+++ app/db/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.settings.config import DB_URL
+from app.db.models import Base
+
+engine = create_engine(DB_URL)
+session_maker = sessionmaker(bind=engine)
+
+
