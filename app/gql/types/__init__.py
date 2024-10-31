9cef533b599f98972c53292292af9665e90be295
chezkyk <Y0548532123@GMAIL.COM>
2024-10-31 17:54:11 +0200

    I added the import here to avoid the problem of circular imports

---
+++ app/gql/types/__init__.py
@@ -0,0 +1,5 @@
+from .country_type import CountryType
+from .city_type import CityType
+from .target_type import TargetType
+from .mission_type import MissionType
+from .target_type_type import TargetTypeType
